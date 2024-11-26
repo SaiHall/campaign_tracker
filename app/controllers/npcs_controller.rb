b54d34617888f179a69872849472772e296eb7a3
Sarah Hall <swatson1994@yahoo.com>
2024-11-26 00:46:39 -0500

    Add npcs index route, view, and controller

---
+++ app/controllers/npcs_controller.rb
@@ -0,0 +1,5 @@
+class NpcsController < ApplicationController
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/", to: "welcome#index"
+
+  get "/npcs", to: "npcs#index"
 end
